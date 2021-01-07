66ccbddb89b11585bb9acd0473c567cf6f73c8d9
Gremln-now <GiesssooCHYT@yandex.ru>
2021-01-07 16:52:59 +0300

    Расстановка кораблей

--- game.py
+++ game.py
@@ -6,9 +6,9 @@ def game_area(area):
     for i in "АБВГДЕЖЗИК":
         str_ += i + " "
     print("\t" + str_)
-    for i in "АБВГДЕЖЗИК":
+    for a in range(10):
         str_ = ""
-        for a in range(10):
+        for i in "АБВГДЕЖЗИК":
             str_ +=str(area[i][a]) + " "
         s += 1
         print(str(s)+ "\t" + str_.rstrip())
@@ -18,7 +18,24 @@ def dictionary():
 str_ = ""
 area_1 = dictionary()
 area_2 = dictionary()
-os.system("clear")
-game_area(area_1)
-print()
-game_area(area_2)
+st = [[4, 1], [3, 2], [2, 3], [1, 4]]
+while st[0][0] != 0 or st[1][0] != 0 or st[2][0] != 0 or st[3][0] != 0:
+    os.system("clear")
+    game_area(area_1)
+    print("""Раставьте корабли на поле
+    Выберите тип корабля:
+    1. 1-х палубный[{}]
+    2. 2-х палубный[{}]
+    3. 3-х палубный[{}]
+    4. 4-х палубный[{}]""".format(st[0][0], st[1][0], st[2][0], st[3][0]))
+    a = int(input("\n..:"))
+    try:
+        for i in range(st[a-1][0]):
+            for s in range(st[a-1][1]):
+                print("Осталось корблей: {}".format(st[a-1][0]))
+                area_1[input("Введите столбец: ").upper()][int(input("Введите строку: "))-1] = "\033[32m\033[1m{}\033[0m".format(a)
+                os.system("clear")
+                game_area(area_1)
+            st[a-1][0] -= 1
+    except KeyError or ValueError:
+        print("Ошибка!!!")
